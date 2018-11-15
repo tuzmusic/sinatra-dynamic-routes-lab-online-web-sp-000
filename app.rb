43b993a6cc6faa77cc80df9c2d1f6e34ed28b8f0
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-15 15:13:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     (params[:number].to_i**2).to_s
   end
 
-  get 'say/:number/:phrase' do
+  get '/say/:number/:phrase' do
     params[:number].times do
       params[:phrase]
     end
