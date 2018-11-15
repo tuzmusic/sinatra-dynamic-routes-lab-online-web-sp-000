b5f557feab35d104d8adaebca88d174e609e5fbd
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-15 15:28:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,7 +30,6 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1]#.to_i
     @num2 = params[:number2]#.to_i
-    binding.pry
     op = ""
     case params[:operation]
     when 'add'
